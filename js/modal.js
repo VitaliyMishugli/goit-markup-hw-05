8f0c1463e7246efae2b24ef733a27d3d74548b06
Vitaliy Mishugli <vmishughli@gmail.com>
2022-05-16 21:23:43 +0100

    Додає модальне вікно та анімації

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.backdropRef.addEventListener("click", logBackdropClick);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+
+// const { height: pageHeaderHeight } = document
+//   .querySelector(".header")
+//   .getBoundingClientRect();
+
+// document.body.style.paddingTop = `${pageHeaderHeight}px`;
